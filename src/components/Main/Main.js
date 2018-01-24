7bbd28860421546a1e80cc512b6530a732ef7e1a
Ba5eem <baseem@gmail.com>
2018-01-24 10:43:08 -1000

    header done

---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+
+
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+        <h1>MAIN JS</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Main from './Main/Main.js';
+import Header from './Header/Header.js';
+
+export { Main, Header };
+
